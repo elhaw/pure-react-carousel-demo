a5cc49551fd17f6700436ba0b03460e8e797e12f
elhaw <abd.elsayed@hotmail.com>
2020-05-01 18:54:32 +0400

    added error and loading handler

--- src/Components/MainSlider/index.js
+++ src/Components/MainSlider/index.js
@@ -15,112 +15,95 @@ import right_arrow from "./work-style/right_arrow.png";
 
 class MainSlider extends Component {
   state = {
-    data :{},
+    data: {},
     error: false,
-    loading:true
-  }
+    loading: true,
+  };
 
   componentDidMount() {
     axios
       .get("https://charity-cms-dev.m3ntorship.net/What-they-say")
-      .then(({data})=>{
-           this.setState({
-             data,
-             error :false,
-             loading:false
-           })
+      .then(({ data }) => {
+        this.setState({
+          data,
+          error: false,
+          loading: false,
+        });
       })
-      .catch((error)=>{
-          this.setState({
-            error:true,
-            loading:false
-          })
-      })
+      .catch((error) => {
+        this.setState({
+          error: true,
+          loading: false,
+        });
+      });
   }
-
   render() {
-    return (
-      <CarouselProvider
-        naturalSlideWidth={500}
-        naturalSlideHeight={250}
-        totalSlides={3}
-      >
-        <FeedBackHeder />
-        <Slider className="sliderWrapper">
-          <Slide index={0}>
-            <figure className="feedback__carousel__quote text-c100 relative bg-c000 py-12 px-24 flex flex-col items-center justify-center">
-              <img
-                className="rounded-full mx-auto"
-                src="https://picsum.photos/90/90"
-                alt="nile"
-              />
-              <blockquote className="feedback__quote__text mt-8 mb-4 text-center">
-                <p className="mb-8 leading-normal font-hairline">
-                  This is due to their excellent service, competitive pricing
-                  and customer support. It’s throughly refresing to get such a
-                  personal touch.
-                </p>
-                <h4 className="text-c200 font-medium text-md font-semibold mb-2">
-                  Christine Rose
-                </h4>
-                <p className="leading-normal text-c600 text-base">Feedback</p>
-              </blockquote>
-            </figure>
-          </Slide>
-          <Slide index={1}>
-            <figure className="feedback__carousel__quote text-c100 relative bg-c000 py-12 px-24 flex flex-col items-center justify-center">
-              <img
-                className="rounded-full mx-auto"
-                src="https://picsum.photos/90/90"
-                alt="nile"
-              />
-              <blockquote className="">
-                <p className="mb-8 leading-normal font-hairline">
-                  This is due to their excellent service, competitive pricing
-                  and customer support. It’s throughly refresing to get such a
-                  personal touch.
-                </p>
-                <h4 className="text-c200 font-medium text-md font-semibold mb-2">
-                  Christine Rose
-                </h4>
-                <p className="leading-normal text-c600 text-base">Feedback</p>
-              </blockquote>
-            </figure>
-          </Slide>
-          <Slide index={2}>
-            <figure className="feedback__carousel__quote text-c100 relative bg-c000 py-12 px-24 flex flex-col items-center justify-center">
-              <img
-                className="rounded-full mx-auto"
-                src="https://picsum.photos/90/90"
-                alt="nile"
-              />
-              <blockquote className="feedback__quote__text mt-8 mb-4 text-center">
-                <p className="mb-8 leading-normal font-hairline">
-                  This is due to their excellent service, competitive pricing
-                  and customer support. It’s throughly refresing to get such a
-                  personal touch.
-                </p>
-                <h4 className="text-c200 font-medium text-md font-semibold mb-2">
-                  Christine Rose
-                </h4>
-                <p className="leading-normal text-c600 text-base">Feedback</p>
-              </blockquote>
-            </figure>
-          </Slide>
-        </Slider>
-        <ButtonBack className="button_back">
-          <img className="" src={left_arrow} />
-        </ButtonBack>
-        <ButtonNext className="button_next">
-          <img className="" src={right_arrow} />
-        </ButtonNext>
-        <DotGroup />
-      </CarouselProvider>
-    );
+    if (this.state.error) {
+      return <div className="error">error here</div>;
+    }
+
+    if (this.state.loading) {
+      return <div className="loading">loading .. </div>;
+    }
+    if (this.state.data.id) {
+      let {
+        Description,
+        Heading: { heading_primary, heading_secondary },
+        testimonials,
+      } = this.state.data;
+
+      let numbersOfSlides = testimonials.length;
+      return (
+        <CarouselProvider
+          naturalSlideWidth={500}
+          naturalSlideHeight={250}
+          totalSlides={numbersOfSlides}
+        >
+          <FeedBackHeader
+            heading_primary={heading_primary}
+            heading_secondary={heading_secondary}
+            description={Description}
+          />
+          <Slider className="sliderWrapper">
+            {testimonials.map((slide, index) => {
+              return (
+                <Slide key ={index} index={{ index }}>
+                  <figure className="feedback__carousel__quote text-c100 relative bg-c000 py-12 px-24 flex flex-col items-center justify-center">
+                    <img
+                      className="rounded-full mx-auto"
+                      src={slide.image.url}
+                      alt="nile"
+                    />
+                    <blockquote className="feedback__quote__text mt-8 mb-4 text-center">
+                      <p className="mb-8 leading-normal font-hairline">
+                        {slide.body}
+                      </p>
+                      <h4 className="text-c200 font-medium text-md font-semibold mb-2">
+                        {slide.image.author}
+                      </h4>
+                      <p className="leading-normal text-c600 text-base">
+                        <a href ={slide.link.url}>{slide.link.text}</a>
+                      </p>
+                    </blockquote>
+                  </figure>
+                </Slide>
+              );
+            })}
+          </Slider>
+          <ButtonBack className="button_back">
+            <img className="" src={left_arrow} />
+          </ButtonBack>
+          <ButtonNext className="button_next">
+            <img className="" src={right_arrow} />
+          </ButtonNext>
+          <DotGroup />
+        </CarouselProvider>
+      );
+    }
   }
 }
 
-class FeedBackHeder extends Component {
+class FeedBackHeader extends Component {
   render() {
     return (
       <section className="feedback bg-c100 relative">
@@ -128,17 +111,13 @@ class FeedBackHeder extends Component {
           <div className="feedback__heading">
             <div className="grid grid-cols-3">
               <h2 className="text-c000 text-xl col-span-2 font-black">
-                What They Are
+                {this.props.heading_primary}
                 <span className="text-c200 border-b-2 font-hairline">
-                  Saying?
+                  {this.props.heading_secondary}
                 </span>
               </h2>
               <div className="text-c700 font-hairline">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur notted adipisicing
-                  elit sed do eiusmod tempor incididunt ut labore et dolore
-                  magna aliqua.
-                </p>
+                <p>{this.props.description}</p>
               </div>
             </div>
           </div>
